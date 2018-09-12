f566013fdce605b4f6677530df1e3f2032db7b10
Jun Zhang <jim.zhang@caicloud.io>
2018-09-12 10:26:01 +0800

    refactor: use internal interfaces rather than export them (#4)

--- status/batch/v1beta1/register.go
+++ status/batch/v1beta1/register.go
@@ -1,7 +1,7 @@
 package v1beta1
 
 import (
-	"github.com/caicloud/rudder-client/status/internal"
+	"github.com/caicloud/rudder-client/status/universal"
 	batchv1beta1 "k8s.io/api/batch/v1beta1"
 )
 
@@ -9,6 +9,6 @@ var (
 	gvkCronJob = batchv1beta1.SchemeGroupVersion.WithKind("CronJob")
 )
 
-func Assist(u internal.Umpire) {
+func Assist(u universal.Umpire) {
 	u.Employ(gvkCronJob, JudgeCronJob)
 }
